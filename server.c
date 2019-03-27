02daa8c6c1efcd6b0565b773fed6708478577da8
R57X08Z9 <Demon_MK97@mail.ru>
2019-03-27 08:51:15 +0300

    server

--- server.c
+++ server.c
@@ -11,12 +11,18 @@
 #include <json.h>
 
 #define SOCKET_PATH "127.0.0.1:9000"
-#define NS_T_A 1
-#define NS_T_TXT 2
+#define MAX_TIME 5
 
 static int socketId; 
 
-//static void state_cb(void *data, int s, int read, int write) {}
+static void answer_a_to_json_array(struct hostent *host, json_object *obj) {
+	char ip[INET6_ADDRSTRLEN];
+	for (int i = 0; host->h_addr_list[i]; i++) {
+		inet_ntop((int)host->h_addrtype, (const void *)host->h_addr_list[i], ip, sizeof(ip));
+		json_object_array_add(obj, json_object_new_string(ip));
+		printf("ip_i = %s\n", ip); // отладочная информация
+	}
+}
 
 static void callback_a(void *arg, int status, int timeouts, unsigned char *abuf, int alen) {
 
@@ -30,31 +36,26 @@ static void callback_a(void *arg, int status, int timeouts, unsigned char *abuf,
 		int status_pars = ares_parse_a_reply(abuf, alen, &host, addrttls, naddrttls);
 		
 		if (status_pars == ARES_SUCCESS) {
-			char ip[INET6_ADDRSTRLEN];
-			for (int i = 0; host->h_addr_list[i]; i++) {
-				inet_ntop((int)host->h_addrtype, (const void *)host->h_addr_list[i], ip, sizeof(ip));
-				json_object_array_add(obj, json_object_new_string(ip));
-				printf("%s\n", ip);
-			}
+			answer_a_to_json_array(host, obj);
 			free(addrttls);
 			free(naddrttls);
 		} else {
-			printf("callback can't parse answer");
-			printf("parse error: %s\n", ares_strerror(status));
+			printf("callback can't parse answer\n");  // необходимая
+			printf("parse error: %s\n", ares_strerror(status)); // ошибка
 		}
 
 		ares_free_hostent(host);
 	} else {
-		printf("query error: %s\n", ares_strerror(status));
+		printf("query faild\n"); // необходимая
+		printf("query error: %s\n", ares_strerror(status)); // ошибка
 	}
 }
 
-static void print_txt(struct ares_txt_reply *txt, void *arg) {
-	if (txt) {
-		struct json_object *obj = arg;
-		json_object_array_add(obj, json_object_new_string(txt->txt));
-		printf("txt_i = %s \n", txt->txt);
-		print_txt(txt->next, arg);
+static void answer_txt_to_json_array(struct ares_txt_reply *txt, struct json_object *obj) {
+	json_object_array_add(obj, json_object_new_string(txt->txt));
+	printf("txt_i = %s\n", txt->txt); // отладочная информация
+	if (txt->next) {
+		answer_txt_to_json_array(txt->next, obj);
 	}
 }
 
@@ -66,33 +67,42 @@ static void callback_txt(void *arg, int status, int timeouts, unsigned char *abu
 		int status_pars = ares_parse_txt_reply(abuf, alen, &txt_out);
 		
 		if (status_pars == ARES_SUCCESS) {
-			print_txt(txt_out, arg);
+			if (txt_out)
+				answer_txt_to_json_array(txt_out, arg);
 		} else {
-			printf("callback can't parse answer");
+			printf("callback can't parse answer"); // необходимая
+			printf("parse error: %s\n", ares_strerror(status)); // ошибка
 		}
 
 		ares_free_data(txt_out);
+	} else {
+		printf("query faild\n"); // необходимая
+		printf("query error: %s\n", ares_strerror(status)); // ошибка
 	}
 }
 
 static void wait_ares(ares_channel channel) {
-	printf("start wait_ares \n");
+	printf("start wait_ares\n"); // необходимая
 	int nfds, count;
 	fd_set readers, writers;
-	struct timeval tv, *tvp;
+	struct timeval tv, *tvp, tvmax;
+	tvmax.tv_sec = MAX_TIME;
 	for (;;) {
 		FD_ZERO(&readers);
 		FD_ZERO(&writers);
 		nfds = ares_fds(channel, &readers, &writers);
 		if (nfds == 0)
 			break;
-		/*поставить ограничени на время выполнения запроса*/
-		tvp = ares_timeout(channel, NULL, &tv);
+		tvp = ares_timeout(channel, &tvmax, &tv);
 		count = select(nfds, &readers, &writers, NULL, tvp);
 		ares_process(channel, &readers, &writers);
 		
 	}
 }
+/*
+int sent_requ(struct json_object *obj, char *dns, char *type) {
+	bool status;
+}*/
 
 int main(void) {
 
@@ -104,73 +114,84 @@ int main(void) {
 
 	status = ares_library_init(ARES_LIB_INIT_ALL);
 	if (status != ARES_SUCCESS) {
-		printf("ares_library_init: %s\n", ares_strerror(status));
+		printf("ares_library init failed\n"); // необходимая
+		printf("ares_library_init: %s\n", ares_strerror(status)); // ошибка
 		return 1;
+	} else {
+		printf("ares_library init complit\n"); // необходимая
 	}
-
-//	options.sock_state_cb = state_cb;
-//	optmask |= ARES_OPT_SOCK_STATE_CB;
 
 	status = ares_init_options(&channel, &options, optmask);
 	if (status != ARES_SUCCESS) {
-		printf("ares_init_options: %s\n", ares_strerror(status));
+		printf("ares_init_options failed\n"); // необходимая
+		printf("ares_init_options: %s\n", ares_strerror(status)); // ошибка
 		return 1;
+	} else {
+		printf("ares_init_options complit\n"); // необходимая
 	}
 
 	/* настройка FastCGI */
 	FCGX_Init();
-	printf("Lib is inited\n");
+	printf("Lib is inited\n"); // необходимая
 	    
 	socketId = FCGX_OpenSocket(SOCKET_PATH, 20);
 	if(socketId < 0) { 
 		return 1;
 	}
  
-	printf("Socket is opened\n");
+	printf("Socket is opened\n"); // необходимая
 	
 	int rc;
 	FCGX_Request request;
 	char *server_name;
 
 	if(FCGX_InitRequest(&request, socketId, 0) != 0) { 
-		printf("Can not init request\n");
+		printf("Can not init request\n"); // необходимая
 		return 1;
 	} 
 
-	printf("Request is inited\n");
+	printf("Request is inited\n"); // необходимая
  
 	for(;;) {
 
-		printf("Try to accept new request\n");
+		printf("Try to accept new request\n"); // необходимая
 		
 		rc = FCGX_Accept_r(&request);
 		if(rc < 0) {
-			printf("Can not accept new request\n");
+			printf("Can not accept new request\n"); // необходимая
 			break;
 		}
 
-		printf("request is accepted\n");
+		printf("request is accepted\n"); // необходимая
 
-		int inSize = atoi(FCGX_GetParam("CONTENT_LENGTH", request.envp));
-		char *str = NULL;
-		str = (char*) realloc(str, inSize + 1);
-		FCGX_GetStr(str, inSize, request.in);
-		str[inSize] = '\0';
+		char *strLenContent = FCGX_GetParam("CONTENT_LENGTH", request.envp);
+		if (!strcmp(strLenContent, "(null)")) {
+			printf("Status: 400 Bad Request\n"); // необходимая
+			FCGX_PutS("Status: 400 Bad Request\r\n", request.out);
+			FCGX_Finish_r(&request);
+			continue;
+		}
+		int inSize = atoi(strLenContent);
+		char *strIn = NULL;
+		strIn = calloc(inSize + 1, sizeof(char));
+		FCGX_GetStr(strIn, inSize, request.in);
+		strIn[inSize] = '\0';
 
-		struct json_object *obj;
-		obj = json_tokener_parse(str);
+		struct json_object *obj, *tmp;
+		obj = json_tokener_parse(strIn);
+		free(strIn);
 
-//		printf("\n---\n%s\n---\n", json_object_to_json_string_ext(obj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY));
+		printf("\n---\n%s\n---\n", json_object_to_json_string_ext(obj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY)); // отладочная
 
-		struct json_object *tmp;
-
+//		struct json_object *tmp;
+		/*вынести в отделный блок*/
 		json_object_object_get_ex(obj, "dns", &tmp);
 		const char *dns_query = json_object_get_string(tmp);
 
 		json_object_object_get_ex(obj, "type", &tmp);
 		const char *type_query = json_object_get_string(tmp);
 
-		printf("get query: DNS = %s, Type = %s", dns_query, type_query);
+		printf("get query: DNS = %s, Type = %s", dns_query, type_query); // необходимая
 
 		tmp = json_object_new_array();
 
@@ -178,7 +199,7 @@ int main(void) {
 			ares_query(channel, dns_query, ns_c_in, ns_t_a, callback_a, tmp);
 		if (!strcmp(type_query, "TXT"))
 			ares_query(channel, dns_query, ns_c_in, ns_t_txt, callback_txt, tmp);
-
+		/*вынести в отделный блок до сюда*/
 		wait_ares(channel);
 
 		// сформировать и вернуть ответ
@@ -186,11 +207,12 @@ int main(void) {
 //		printf("\n---\n%s\n---\n", json_object_to_json_string_ext(obj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY));
 		FCGX_PutS("Content-type: application/json\r\n", request.out);
 		FCGX_PutS("\r\n", request.out);
-		FCGX_PutS(json_object_to_json_string(obj), request.out);
+		FCGX_PutS(json_object_to_json_string_ext(obj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY), request.out);
 		//закрыть текущее соединение
 		FCGX_Finish_r(&request);
+		printf("query complit"); // необходимая
 	} 
 
-	printf("\nexit");
+	printf("\nexit"); // необходимая
 	return 0; 
 }
